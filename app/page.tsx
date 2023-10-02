af0f31e7c7a692ffdb8bd96f49a218b7f20c6195
abhiCS7A60 <88727740+abhinv98@users.noreply.github.com>
2023-10-02 22:36:09 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import HeroCarousel from "@/components/HeroCarousel";
+import Searchbar from "@/components/Searchbar";
 import Image from "next/image";
 
 const Home = () => {
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+      <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
@@ -23,7 +25,17 @@ const Home = () => {
               Powerful, self-serve product and growth analytics to help you
               convert, engage, and retain more.
             </p>
+            <Searchbar />
           </div>
+          <HeroCarousel/>
+        </div>
+      </section>
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {["AppleIphone", "book", "sneaker"].map((product) => (
+            <div>{product}</div>
+          ))}
         </div>
       </section>
     </>
