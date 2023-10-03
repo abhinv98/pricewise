3542f060dc6fffbfe29c3f25cfb86c11c0aa47d1
abhiCS7A60 <88727740+abhinv98@users.noreply.github.com>
2023-10-03 18:37:46 +0530

    .

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,12 +1,12 @@
 "use server";
 
 import axios from "axios";
-import * as cheerio from "cheerio";
-import { extractCurrency, extractPrice } from "../utils";
+
+import cheerio from "cheerio";
+import { extractCurrency, extractDescription, extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
-
   const username = String(process.env.BRIGHT_DATA_USERNAME);
   const password = String(process.env.BRIGHT_DATA_PASSWORD);
   const port = 22225;
@@ -25,6 +25,7 @@ export async function scrapeAmazonProduct(url: string) {
     const $ = cheerio.load(response.data);
 
     const title = $("#productTitle").text().trim();
+    const description = extractDescription($);
     const currentPrice = extractPrice(
       $(".priceToPay span.a-price-whole"),
       $("a.size.base.a-color-price"),
@@ -55,19 +56,31 @@ export async function scrapeAmazonProduct(url: string) {
 
     const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "");
 
+    const reviewCount = $("#acrCustomerReviewText")
+      .text()
+      .split(" ")[0]
+      .replace(",", "");
+
+    const starsRaw = $("#acrPopover .a-icon-alt").text();
+    const stars = parseFloat(starsRaw.split(" ")[0]);
+    
     const data = {
       url,
       currency: currency,
       image: imageUrls[0],
       title,
-      currentPrice: Number(currentPrice),
-      originalPrice: Number(originalPrice),
+      description,
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
       priceHistory: [],
       discountRate: Number(discountRate),
       category: "category",
-      reviewsCount: 100,
-      stars: 4.5,
+      reviewsCount: Number(reviewCount),
+      stars: stars,
       isOutOfStock: outOfStock,
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      average: Number(currentPrice) || Number(originalPrice),
     };
 
     console.log(data);
